381a49e596b306afe6cfeb674579d640f6bd5704
PizzaCoven <46539400+PizzaCoven@users.noreply.github.com>
2021-03-16 02:32:54 -0400

    week 10 project

--- week10/components/Name.js
+++ week10/components/Name.js
@@ -18,20 +18,3 @@ export default class Name extends Component {
   );
 }
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
